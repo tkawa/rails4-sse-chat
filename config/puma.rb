f5dad47db08be2b64eb22ee2e11f85c1538f81d3
Toru KAWAMURA <tkawa@4bit.net>
2013-03-16 18:33:30 +0900

    Not use config/puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -2,4 +2,4 @@ require "active_record"
 cwd = File.dirname(__FILE__)+"/.."
 ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
 ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RACK_ENV"]])
-ActiveRecord::Base.verify_active_connections!
+#ActiveRecord::Base.verify_active_connections!
